2268bed77b01d47674d08a26f036d8c3e3ecc8a2
Itai Zur <111233236+itai-zur@users.noreply.github.com>
2023-03-06 17:26:35 +0200

    Update rds.tf

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_rds_cluster" "app1-rds-cluster" {
   cluster_identifier      = "app1-rds-cluster"
-  storage_encrypted       = true
+  storage_encrypted       = false
   allocated_storage       = 10
   backup_retention_period = 0
   tags = {
